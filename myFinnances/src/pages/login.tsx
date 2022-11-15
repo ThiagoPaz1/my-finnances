e7599dd5044f355e729da0c5f6a18a03e580bac9
Thiago da Paz <thiagopazjunior@gmail.com>
2022-11-15 15:46:28 -0300

    refactor: Ajuste de validações de tela de login.

--- myFinnances/src/pages/login.tsx
+++ myFinnances/src/pages/login.tsx
@@ -1,16 +1,16 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import '../css/login.css'
 import logo from '../assets/financa.png';
 import { useForm } from "react-hook-form";
 import { yupResolver } from '@hookform/resolvers/yup';
-import {object, string} from 'yup';
-import {validEmail, validPassword} from "../utils/regex"
+import { object, string } from 'yup';
+import { validEmail, validPassword } from "../utils/regex"
 
 
 interface IFildForm {
-    email: string,
-    password: string,
-  }
+  email: string,
+  password: string,
+}
 
   const schema = object ({
     email: string().required("Campo obrigatório."),
@@ -19,79 +19,89 @@ interface IFildForm {
 
 export default function login() {
 
-    const { register, handleSubmit: onSubmit, watch, formState: { errors } } = useForm({resolver: yupResolver(schema)});
+  const { register, handleSubmit: onSubmit, watch, formState: { errors } } = useForm({ resolver: yupResolver(schema) });
 
-    // console.log(errors);
-    
-
-    const [fildsForm, setFildsForm] = useState<IFildForm>({email: "", password:""});
+  // console.log(errors);
 
 
-    const [emailError, setEmailError] = useState(false);
-    const [passwordError, setPasswordError] = useState(false);    
+  const [fildsForm, setFildsForm] = useState<IFildForm>({ email: "", password: "" });
 
-    const validate = () => {
-        if(!validEmail.test(fildsForm.email)){
-            setEmailError(true);
-            console.log("Entrei");
-        } else{
-            setEmailError(false);
-            console.log("Entrei");
-        }
 
-        if(!validPassword.test(fildsForm.password)){
-            setPasswordError(true);
-            console.log("Entrei");
-        } else{
-            setPasswordError(false);
-            console.log("Entrei");
-        }
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+
+  const validate = () => {
+    if (!validEmail.test(fildsForm.email)) {
+      setEmailError(true);
+      console.log("Entrei");
+    } else {
+      setEmailError(false);
+      console.log("Entrei");
     }
 
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) =>{
-    const {name, value} = event.target;
-    setFildsForm({...fildsForm, [name]: value});
+    if (!validPassword.test(fildsForm.password)) {
+      setPasswordError(true);
+      console.log("Entrei");
+    } else {
+      setPasswordError(false);
+      console.log("Entrei");
     }
+  }
 
-    const handleSubmit = (data: any) => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFildsForm({ ...fildsForm, [name]: value });
+  }
 
-        console.log(data);
-        setFildsForm({email: "", password: ""});
-    }
+  const handleSubmit = (data: any) => {
 
-    return(
+    console.log(data);
+    setFildsForm({ email: "", password: "" });
+  }
+
+  return (
     <div>
-        <div className="container">
-            <header>
-                <img className='logo' src={logo} alt="logo-finanças"/>
-                <h1>Bem vindo</h1>
-            </header>
+      <div className="container">
+        <header>
+          <img className='logo' src={logo} alt="logo-finanças" />
+          <h1>Bem vindo</h1>
+        </header>
         {/* <input type="button" value={count} onClick={() => setCount(count + 1)}/> */}
 
         <section>
-        <form className='form' onSubmit={onSubmit(handleSubmit)}>
+          <form className='form' onSubmit={onSubmit(handleSubmit)}>
             <div className="email">
-                <label htmlFor="">Usuário</label>
-                <input type="email" placeholder='Digite seu email' {...register("email")} value={fildsForm.email} onChange={handleChange}/>
-                <span className='error'>{errors?.email?.message}</span>
+              <label htmlFor="">Usuário</label>
+              <input
+                type="email"
+                placeholder='Digite seu email'
+                value={fildsForm.email}
+                {...register("email")}
+                onChange={handleChange}
+              />
+              <span className='error'>{errors?.email?.message}</span>
             </div>
-            {emailError && <p>Email incorreto!</p>}
 
             <div className="password">
-                <label htmlFor="">Senha</label>
-                <input type="password" placeholder='Digite sua senha' {...register("password")} value={fildsForm.password} onChange={handleChange}/>
-                <span className='error'>{errors?.password?.message}</span>
+              <label htmlFor="">Senha</label>
+              <input
+                type="password"
+                placeholder='Digite sua senha'
+                {...register("password")}
+                onChange={handleChange}
+                value={fildsForm.password}
+              />
+              <span className='error'>{errors?.password?.message}</span>
             </div>
-            {passwordError && <p>Password incorreto!</p>}
             <a className='esqueceu-senha' href="#">Esqueceu a senha?</a>
-
-            <input className='submit' type="submit" value="Entrar" onClick={validate}/>
-        </form>
-        <div className="new-account">
+            
+            <input className='submit' type="submit" value="Entrar" onClick={validate} />
+          </form>
+          <div className="new-account">
             <h2>Ainda não tem uma conta? <a href="#">Criar conta</a></h2>
-        </div>
+          </div>
         </section>
-        </div>
+      </div>
     </div>
-    )
+  )
 }
